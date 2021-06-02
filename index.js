5bb604d0aa8878323eeff66eb228640a961331d5
jungleberrytv <jungleberrytv@gmail.com>
2021-06-02 09:54:08 -0500

    Updated command files

--- index.js
+++ index.js
@@ -20,13 +20,12 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
-
-	if (!client.commands.has(command)) return;
+	if (!client.commands.has(commandName)) return;
 
 	try {
-		client.commands.get(command).execute(message, args);
+		command.execute(message, args);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
